79c893e878cb19727d9d6f8b9fe1b9af0622f71a
Andriy Hafyak <x3x7x3@gmai.com>
2017-01-18 19:28:40 +0100

    Module05

---
+++ src/Module06/User.java
@@ -0,0 +1,23 @@
+package Module06;
+
+/**
+ * Created by user on 18.01.2017.
+ */
+public class User {
+
+
+    long id;
+    String firstName;
+    String lastName;
+    int salary;
+    int balance;
+
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+}
