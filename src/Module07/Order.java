eaf2271e1eff9cf6999564f45efd72c325a2b5e8
Andriy Hafyak <x3x7x3@gmai.com>
2017-02-05 12:30:19 +0100

    Module05

---
+++ src/Module07/Order.java
@@ -0,0 +1,17 @@
+package Module07;
+
+/**
+ * Created by user on 04.02.2017.
+ */
+
+ import Module04.Currency;
+
+public class Order {
+    long id;
+    int price;
+    Currency currency;
+    String itemName;
+    String shopIdentificator;
+    User user;
+
+}
---
+++ src/Module07/User.java
@@ -0,0 +1,12 @@
+package Module07;
+
+/**
+ * Created by user on 04.02.2017.
+ */
+public class User {
+    long id;
+    String firstName;
+    String lastName;
+    String city;
+    int balance;
+}
